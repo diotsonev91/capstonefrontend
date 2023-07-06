e39e39d82e4b5bf42cc75c063e99514938321031
Deyan Tsonev <dio.tsonev@gmail.com>
2023-07-06 11:12:43 +0300

    fix

--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Header = () => {
-  return <header>{/* Header content goes here */}</header>;
-};
-
-export default Header;
--- src/components/Main.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Main = () => {
-  return <main>{/* Main content goes here */}</main>;
-};
-
-export default Main;
